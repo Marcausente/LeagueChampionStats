4e4163355a5475738756bd635c455cc943402aed
Marc Fernández <marcausente@gmail.com>
2025-01-02 18:59:31 +0100

    Initial Commit

---
+++ LeagueChampionStats/LeagueChampionStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeagueChampionStatsApp.swift
+//  LeagueChampionStats
+//
+//  Created by Marc Fernández on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct LeagueChampionStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
