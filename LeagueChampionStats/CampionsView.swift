852a00dadd83d46ab2a4d13e689a21a1783179cd
Marc Fernández <marcausente@gmail.com>
2025-01-02 19:09:17 +0100

    First structure

---
+++ LeagueChampionStats/CampionsView.swift
@@ -0,0 +1,18 @@
+//
+//  CampionsView.swift
+//  LeagueChampionStats
+//
+//  Created by Marc Fernández on 2/1/25.
+//
+
+import SwiftUI
+
+struct CampionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CampionsView()
+}
---
+++ LeagueChampionStats/ItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemsView.swift
+//  LeagueChampionStats
+//
+//  Created by Marc Fernández on 2/1/25.
+//
+
+import SwiftUI
+
+struct ItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemsView()
+}
