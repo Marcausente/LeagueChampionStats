fa670b20b5c7d330450ec73c06217b4320090192
Marc Fernández <marcausente@gmail.com>
2025-01-03 16:47:44 +0100

    ChampionsNav buttons v1

---
+++ LeagueChampionStats/MagesView.swift
@@ -0,0 +1,18 @@
+//
+//  MagesView.swift
+//  LeagueChampionStats
+//
+//  Created by Marc Fernández on 3/1/25.
+//
+
+import SwiftUI
+
+struct MagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MagesView()
+}
